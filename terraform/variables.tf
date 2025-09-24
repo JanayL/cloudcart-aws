74a7f57db0dc39cf9c9e9121210d2ffe71d3eb50
JanayL <leejanay23@gmail.com>
2025-09-23 21:30:49 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "cloudcart"
+}
+
+variable "docker_image" {
+  type    = string
+  # your image on Docker Hub
+  default = "jalee12/cloudcart:latest"
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 2
+}
+
+variable "max_size" {
+  type    = number
+  default = 4
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
